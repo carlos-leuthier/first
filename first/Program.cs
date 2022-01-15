3ebec4617ddd1cf7debffebe6aa2196d98378be6
carlo <carlosleuthier3@gmail.com>
2022-01-15 01:25:01 -0300

    Adicionar arquivos de projeto.

---
+++ first/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace first
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            var a = "hello, ";
+            var b = "world!";
+Console.WriteLine(a + b);
+        }
+    }
+}
